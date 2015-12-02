3f4ec037549185cdc5deb17ff8cabeb22d34186d
eximus <fabio_r11@hotmail.com>
2015-12-02 02:58:50 +0000

    API wroking. Add more try/except blocks Maybe has a bug when fetching download link, fails ocasionally.

--- torrent_api.py
+++ torrent_api.py
@@ -1,12 +1,12 @@
 #! /usr/bin/python2
 
-''' 
-Frontend application for torrent searching and download for Kickass 
+'''
+Frontend application for torrent searching and download for Kickass
 Created - 24.11.15
 Copyright (C) 2015 - eximus
 '''
 
-import urllib2, urllib, sys, re, os, requests
+import urllib2, urllib, sys, re, requests, os
 from sgmllib import SGMLParser
 
 KICKASS = "http://kickass.unblocked.la/"
@@ -17,21 +17,25 @@ class URL_lister(SGMLParser):
 	# overload reset on SGMLParser
 	def reset(self):
 		SGMLParser.reset(self)
-		self.url= [] 
+		self.url= []
 	def start_a(self, attrs):
 		self.url.extend([y for (x,y) in attrs if x == 'href'])
 	def start_td(self, attrs):
-		pass						
+		pass
 
 def build_url(main_url, search_term, order_results):
 	search_url = ''
 	search_url = (main_url + 'usearch/' + urllib2.quote(search_terms, safe='') + order_results)
 	print "\033[0;32mPage Link: \033[0m%s" % search_url
 	return search_url
-	
+
 def parse_page_links(url, parser=URL_lister()):
 	request = urllib2.Request(url, headers=HEADER)
-	page = urllib2.urlopen(request)
+	try:
+		page = urllib2.urlopen(request)
+	except urllib2.URLError:
+		print "\033[1;31mName or Service Unknown:\033[0m %s" % url
+		return []
 	content = page.read() # Retrieve HTML
 	parser.feed(content) # Parse content
 	parser.close()
@@ -39,14 +43,14 @@ def parse_page_links(url, parser=URL_lister()):
 	returned_urls = parser.url
 	parser.reset()
 	return returned_urls
-	
+
 def search(url, parser=URL_lister(), allow_magnets = False):
 	link_list = []
 	regex = '.*\.html' + ('|magnet:?.*' if allow_magnets else '')
 	match_pattern = re.compile(regex)
 	print "\033[0;32mSearching...\033[0m"
 	links = parse_page_links(url, parser)
-	
+
 	# search links
 	for link in links:
 		matches = match_pattern.findall(link)
@@ -58,23 +62,25 @@ def search(url, parser=URL_lister(), allow_magnets = False):
 	return link_list
 
 def download_file(url, fname):
-    # http://stackoverflow.com/a/14114741/1302018
-    try:
-        r = requests.get(url, stream=True, headers=HEADER)
-        with open(fname, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=1024):
-                if chunk:
-                    f.write(chunk)
-                    f.flush()
-    except requests.exceptions.RequestException as e:
-        print('\n' + str(e))
-        sys.exit(1)
- 
-    return fname
+	if os.path.exists('torrent/'):
+		os.mkdir('torrents/')
+	fname = 'torrents/' + fname
+	try:
+		r = requests.get(url, stream=True, headers=HEADER)
+		with open(fname, 'wb') as f:
+			for chunk in r.iter_content(chunk_size=1024):
+				if chunk:
+					f.write(chunk)
+					f.flush()
+	except requests.exceptions.RequestException as e:
+		print '\n' + str(e)
+		sys.exit(1)
+	return fname
 
 def download_torrents(host_url_list):
 	link_list = []
 	links = []
+	file_list = []
 	matches = []
 	cont = 1
 	select_pattern = 'https.*torcache.*'
@@ -100,10 +106,9 @@ def download_torrents(host_url_list):
 		filename = link.split("]")[-1]
 		print "\r\033[0;32mDownloading File [%d / %d]\033[0m" % (cont, len(link_list)),
 		sys.stdout.flush()
-		download_file(link, filename)
+		file_list.append(download_file(link, filename))
 		cont = cont + 1
-	return link_list
-	
+	return file_list
 
 if __name__ == '__main__':
 	try: # Input
