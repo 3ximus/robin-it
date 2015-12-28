59b76d2a53105c2cd071377552f1477b63282930
eximus <fabio_r11@hotmail.com>
2015-12-28 19:21:18 +0000

    v1.0.1 Minor noticeable improvements

--- README.md
+++ README.md
@@ -3,22 +3,59 @@
 
 Python web crawler to automaticly search and gather web resources.
 
-## TV Show Search Engine v1.0:
- 	Searches for available tv shows given keyword, quality (optional), season and episodeS (optional)
-	Usage:
-		`$ ./tv_crawler.py`
-		If Quality left blank defaults to 720p
-		If Episodes left blank defaults to 10 episodes `range(11)[1:]`
-	. Multiprocess Test version on another branch
+--------------------------------
 
-## Other tools:
-	Search torrents
-	List url from page
+##TV Show Search Engine v1.0.1##
+
+Searches for available tv shows given keyword, quality (optional), season and episodeS (optional)
+
+Usage:
+
+	. `$ ./tv_crawler.py`
+	. If Quality left blank defaults to 720p
+	. If Episodes left blank defaults to 10 episodes `range(11)[1:]`
+*Multiprocess Test version on another branch*
+
+--------------------------------
+
+##Other tools##
+
+Search torrents
+
+List url from page
+
+Benchmark Page parsers
+
+--------------------------------
 
 ## TODO:
-	- Implement a beautiful soup parser
-	- Implement custom parser
+
+v1.1: (Speed and Usability Upgrades)
+
+	- Implement a beautiful soup parser (DONE)
+	- Implement custom parser (DISCONTINUED)
 	- Remove the get download links and use the search algorithm to retrive the magnets and torrent links
-	- 
+	- Implement `parse_page_links_2` to use with any parser. Also make it receive content instead of making it get the page (DONE)
+
+v1.2: (Info Display)
+
+	- Get TV show info from a web page
+
+v1.3: (Traking System)
+
+	- Tv Show tracking 
+
+v1.4: (Big Release)
+
+	- Incorporate Traking with Search/Download engine
+
+v2.0:
+	- Implement GUI
+
+###MOVIES###
+
+--------------------------------
+
+##Known Limitations##
 
 
--- torrent_api.py
+++ torrent_api.py
@@ -37,6 +37,96 @@ class URL_lister(SGMLParser):
 		pass
 
 '''
+Torrent data structure
+'''
+class Torrent():
+	def __init__(self, name = '', link = '', magnet = '', tor_file = '', seeds = '', peers = '', age = '', files = '', size = '', host = KICKASS):
+		self.name = name
+		self.link = link
+		self.magnet = magnet
+		self.tor_file = tor_file
+		self.size = size
+		self.files = files
+		self.age = age
+		self.seeds = seeds
+		self.peers = peers
+		self.host = host
+	def to_string(self):
+		print "name: %s" % self.name
+		print "link: %s" % self.link
+		print "magnet: %s" % self.magnet
+		print "torrent file: %s" % self.tor_file
+		print "size: %s" % self.size
+		print "files: %s" % self.files
+		print "age: %s" % self.age
+		print "seeds: %s" % self.seeds
+		print "peers: %s" % self.peers
+		print "host: %s" % self.host
+		print " ==== "
+
+
+'''
+Beautiful Soup 4
+Parser for BeautifulSoup 4
+Returns a tuple with Torrent class
+Maybe slower compared to URL_lister but the information is already processed and ready to be used
+'''
+class BS4():
+	
+	def __init__(self):
+		self.parsed = []
+		self.btree = None # not an actual btree but a 'beautiful tree'
+
+	''' 
+	Callable feed method
+	Receives the content to parse, or the open filedescriptor and how to parse it
+	'''
+	def feed(self, html, query_for='torrent'):
+		self.btree = BeautifulSoup(html, "lxml") # parse the webpage with lxml parser
+		if query_for == 'torrent': self._torrent_parsing(host = KICKASS) # TODO pass another host if needed
+		else: raise ValueError("Unknown parser option given")
+
+	''' Prepare for parsing according to host structure '''
+	def _torrent_parsing(self, host = None):
+		if not host: raise ValueError("No valid host was given")
+		elif host == KICKASS: self._torrent_parsing_kat() # host is kickass so call a function to handle it
+		else : raise ValueError("Unknown host given")
+
+	''' Parse acording to Kickass structure '''
+	def _torrent_parsing_kat(self):
+		tm_pattern = re.compile('Download torrent file|Torrent magnet link') # torrent and magnets pattern
+		# looking at the html all the information about the torrent can be found as follows
+		# torrent_ids comes as a list of tuples being the first element the torrent name and the last its link
+		# torren_files and magnets is a list of torrent links and magnets respectivly
+		# torrent_info comes as: size, files, age, seeds, peers. Repeated for each torrent
+		torrent_ids = [(r.get_text(), r.get('href')) for r in self.btree.find_all('a', attrs = {'class', 'cellMainLink'})]
+		tm_links = [r.get('href') for r in self.btree.find_all('a', {'title' : tm_pattern})]
+		torrent_info = [r.get_text() for r in self.btree.find_all('td', {'class', 'center'})]
+
+		# get everything nice and sorted as list of Torrent structures
+		# compensate the fact that links and info are sequentialy on the list
+		for i, n in enumerate(torrent_ids):
+			to_add = Torrent(name = n[0],
+							 link = n[1],
+							 magnet = tm_links[2*i + 0],
+							 tor_file = tm_links[2*i + 1],
+							 size = torrent_info[5*i + 0],
+							 files = torrent_info[5*i + 1],
+							 age = torrent_info[5*i + 2],
+							 seeds= torrent_info[5*i + 3],
+							 peers = torrent_info[5*i + 4])
+			self.parsed.append(to_add)
+		return self.parsed
+
+	''' Reset class attributes '''
+	def reset(self):
+		self.parsed = None
+		self.btree = None
+
+	def close(self):
+		pass
+
+'''
 Build Search URL
 Receives a string and the page URL to do the search and builds the search url for that page.
 Receives the order in wich you want the results to be returned, accepted values are 'seeds' and 'age'.
