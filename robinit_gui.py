8829b141e875731503183d5b0669e96eddac0dbf
eximus <fabio_r11@hotmail.com>
2016-01-11 20:14:10 +0000

    Update: Structure improvements Habilty to select shows with a selector class

--- robinit_gui.py
+++ robinit_gui.py
@@ -6,8 +6,6 @@ Created - 30.12.15
 Copyright (C) 2015 - eximus
 '''
 
-import sys
-
 from kivy.app import App
 from kivy.lang import Builder # to import .kv file
 from kivy.clock import Clock
@@ -17,10 +15,12 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.screenmanager import Screen, ScreenManager, SlideTransition
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
-from kivy.uix.behaviors import ButtonBehavior
-from kivy.uix.image import Image
+from kivy.uix.bubble import Bubble
 from kivy.uix.image import AsyncImage
-from kivy.properties import ListProperty, NumericProperty, ObjectProperty
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.event import EventDispatcher
+from kivy.animation import Animation
+from kivy.properties import ListProperty, NumericProperty, BooleanProperty
 from random import random, randint
 from functools import partial
 
@@ -37,6 +37,10 @@ B_MAX_OPACITY = 0.3 # maximum opacity a bokeh can reach
 B_OPACITY_RATE = 0.04 # rate of opacity change, less == more screen time
 B_MIN_SIZE = 20 # minimum bookeh size
 B_MAX_SIZE = 150 # maximum bokeh size
+
+# --- SELECTION BAR GLOBALS ---
+S_BAR_SIZE = 80
+S_MOVEMENT_TIME = 0.2
 
 # ------------------------------
 #         BACKGROUND
@@ -109,26 +113,88 @@ class Background(FloatLayout):
 		self.add_widget(new_bokeh) # actually spawn the bockeh
 
 
+# ------------------------------------
+#               EVENTS
+# ------------------------------------
+class MultiEventDispatcher(EventDispatcher):
+	def __init__(self, **kwargs):
+		self.register_event_type('on_selection')
+		super(MultiEventDispatcher, self).__init__(**kwargs)
+
+	'''
+	Function called on the event dispatcher to trigger a on_selection event
+	Params  should be a s follows:
+		what_was_selected: instance of gui selection
+		linked_content: instance of content that the gui "points" to
+		value: True if seleced, False if unselected
+	'''
+	def select(self, what_is_selected, linked_content , value):
+		self.dispatch('on_selection', what_is_selected, linked_content, value)
+
+	''' Default handler for the on_selection event '''
+	def on_selection(self, what_is_selected, linked_content, value, *args):
+		pass
+
+	# GENERATE EVENT DISPATCHERS
+
+event_manager = MultiEventDispatcher()
+
+# ------------------------------------
+#             SHOWS GRID
+# ------------------------------------
+
 class ImagePoster(ButtonBehavior, AsyncImage):
 	pass
 
-class ImageBanner(ButtonBehavior, AsyncImage):
+class Placeholder:
+	pass
 
-	''' Set on_press action'''
+class ImageBanner(ButtonBehavior, AsyncImage):
+	selected = BooleanProperty(False)
+	linked_content = Placeholder()
+
+	''' When Image is pressed '''
 	def on_press(self):
-		pass
+		if self.selected > 0:
+			self.selected = False
+			event_manager.select(self, self.linked_content, False) # launch selected event
+		else:
+			self.selected = True
+			event_manager.select(self, self.linked_content, True) # launch selected event
+
+	'''
+	Catch self.selected change event
+	Params:
+		instance: represents self
+		value: selection value, True if selected, False if unselected
+	'''
+	def on_selected(self, instance, value):
+# TODO instead of colouring use rectangle in the canves BUT DONT FUCKING FORGET TO PUT
+# pos: self.pos , OTHERWISE IT **FUCKING** WONT MOVE!!!
+		if value: self.select_effect()
+		else: self.unselect_effect()
+
+	''' Apply effect of selected '''
+	def select_effect(self):
+		self.color = [0.5, 0.5, 0.5, 1]
+
+	''' Apply effect of selected '''
+	def unselect_effect(self):
+		self.color = [1, 1, 1, 1]
+
 
 class ShowsGrid(GridLayout):
 	def __init__(self, **kwargs):
 		super(ShowsGrid, self).__init__(**kwargs)
 		# Make sure the height is such that there is something to scroll.
 		self.bind(minimum_height=self.setter('height'))
-		for i in range(30):
+		# generate grid content
+		for i in range(17):
 			img = ImageBanner(
 				source='http://thetvdb.com/banners/graphical/262407-g11.jpg')
 			self.add_widget(img)
 
-	''' Sets amount of columns when layout streches '''
+	''' Sets amount of columns when layout size is changed (resize) '''
 	def on_size(self, *largs):
 		if self.right < 650: self.cols = 1
 		elif self.right < 980: self.cols = 2
@@ -136,18 +202,135 @@ class ShowsGrid(GridLayout):
 
 ''' Contains show information '''
 class ItemScroller(ScrollView):
+	pass
+
+# ------------------------------
+#            SELECTOR
+# ------------------------------
+
+class SelectionMenuBar(Widget):
 	def __init__(self, **kwargs):
-		super(ItemScroller, self).__init__(**kwargs)
+		super(SelectionMenuBar, self).__init__(**kwargs)
+
+class ShowSelectionMenuBar(SelectionMenuBar):
+	def __init__(self, **kwargs):
+		super(ShowSelectionMenuBar, self).__init__(**kwargs)
+		butt1 = RemoveShow()
+		butt2 = ToogleWatched()
+		self.add_widget(butt1)
+		self.add_widget(butt2)
+
+class Selector(FloatLayout):
+
+	selected_list = ListProperty(0) # amount of selected items
+	s_raised = BooleanProperty(False) # selection menu raised
+
+	def __init__(self, **kwargs):
+		super(Selector, self).__init__(**kwargs)
+		# on_selection event on the event_manager will trigger a handle_selection
+		event_manager.bind(on_selection=self.handle_selection)
+		# add a hidden selection menu
+		self.select_menu_bar = ShowSelectionMenuBar(pos=(0, -S_BAR_SIZE), size_hint_y=None, height=S_BAR_SIZE)
+		self.add_widget(self.select_menu_bar)
+
+	'''
+	Triggered when a on_selection event ocurrs
+	Params:
+		event_instance: instance of MultiEventDispatcher
+		what_was_selected: instance of what was selected in the gui
+		linked_content: instance of linked content to gui element
+		value: selection value, True if selected, False if unselected
+		*args: should be empty
+	'''
+	def handle_selection(self, event_instance, what_was_selected, linked_content, value, *args):
+		if value: self.selected_list.append((what_was_selected, linked_content))
+		else: self.selected_list.remove((what_was_selected, linked_content))
+
+	'''
+	Called when self.selected_list content is changed
+	Sets self.s_raised in so that a menu is raised / lowered
+	Params:
+		instance: represents self
+		value: list with new content
+	'''
+	def on_selected_list(self, instance, value):
+		if len(value) == 0: self.s_raised = False
+		elif not self.s_raised: self.s_raised = True
+
+	'''
+	Called when self.s_raised value is changed
+	Used to raise/lower selection menu
+	Params:
+		instance: represents self
+		value: new value
+	'''
+	def on_s_raised(self, instance, value):
+		if value == True: self.raise_selection_menu()
+		else: self.lower_selection_menu()
+
+	''' Raises the selection menu '''
+	def raise_selection_menu(self):
+		Animation.cancel_all(self.select_menu_bar)
+		anim = Animation(x=0, y=0, duration=S_MOVEMENT_TIME)
+		anim.start(self.select_menu_bar)
+
+	''' Raises the selection menu '''
+	def lower_selection_menu(self):
+		Animation.cancel_all(self.select_menu_bar)
+		anim = Animation(x=0, y=-S_BAR_SIZE, duration=S_MOVEMENT_TIME)
+		anim.start(self.select_menu_bar)
+
+	''' Clear everything selected selections '''
+	def clear_selected(self):
+		for tup in self.selected_list:
+			tup[0].selected = False
+		self.selected_list = []
+
+class SingleSelector(Selector):
+	'''
+	Triggered when a on_selection event ocurrs
+	This overloads the correspondent super function
+	Params:
+		event_instance: instance of MultiEventDispatcher
+		what_was_selected: instance of what was selected in the gui
+		linked_content: instance of linked content to gui element
+		value: selection value, True if selected, False if unselected
+		*args: should be empty
+	'''
+	def handle_selection(self, event_instance, what_was_selected, linked_content, value, *args):
+		if value:
+			self.clear_selected()
+			self.selected_list.append((what_was_selected, linked_content))
+		else: self.clear_selected()
+
+# ------------------------------
+#            BUTTONS
+# ------------------------------
+
+class ThemeButton(Button):
+	pass
+
+class RemoveShow(ThemeButton):
+	pass
+
+class ToogleWatched(ThemeButton):
+	pass
 
 # ------------------------------
 #            SCREENS
 # ------------------------------
 
 class ShowsMainScreen(Screen):
-	pass
+	def __init__(self, **kwargs):
+		super(ShowsMainScreen, self).__init__(**kwargs)
+		selector = Selector()
+		self.add_widget(selector)
 
 class AllShowsScreen(Screen):
-	pass
+	def __init__(self, **kwargs):
+		super(AllShowsScreen, self).__init__(**kwargs)
+		selector = SingleSelector()
+		self.add_widget(selector)
 
 class MoviesMainScreen(Screen):
 	pass
