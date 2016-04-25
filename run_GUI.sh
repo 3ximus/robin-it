c59787d319b3deabcb78b8df90051318d4bac6cb
eximus <fabio_r11@hotmail.com>
2016-04-25 03:33:47 +0100

    Make this console only

--- run_GUI.sh
+++
@@ -1,9 +0,0 @@
-if [ -z "$1" ]; then
-	echo "Starting in Window Mode..."
-	./robinit_gui.py --size=1100x600  # Run in window size
-elif [ "$1" == "-f" ]; then
-	./robinit_gui.py --auto-fullscreen # Run as fullscreen
-elif [ "$1" == "-b" ]; then
-	./robinit_gui.py --size=1100x600 --fake-fullscreen # Run in window size borderless
-fi
-
