4a52845903da3f46d185ff50a9202031f7d0c830
eximus <fabio_r11@hotmail.com>
2016-04-26 21:44:25 +0100

    added setup script

---
+++ setup.py
@@ -0,0 +1,19 @@
+from cx_Freeze import setup, Executable
+
+#Run with python2 setup.py build
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict(packages = [], excludes = [])
+
+base = 'Console'
+
+executables = [
+    Executable('robinit_console.py', base=base, targetName = 'robinit_console_v1.3')
+]
+
+setup(name='robinit',
+      version = '1.3',
+      description = 'TV Show Tracker and downloader -- console version',
+      options = dict(build_exe = buildOptions),
+      executables = executables)
