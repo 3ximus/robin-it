97e5f7a524711742e3af3398aab5a3114e92f720
eximus <fabio_r11@hotmail.com>
2016-04-29 23:22:30 +0100

    added more build types to setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,29 @@
-from cx_Freeze import setup, Executable
+from sys import platform as _platform
+if _platform =="linux" or _platform == "linux2":
+	from cx_Freeze import setup, Executable
 
-#Run with python2 setup.py build
+	#Run with python2 setup.py build
 
-# Dependencies are automatically detected, but it might need
-# fine tuning.
-buildOptions = dict(packages = [], excludes = [])
+	# Dependencies are automatically detected, but it might need
+	# fine tuning.
+	buildOptions = dict(packages = [], excludes = [])
 
-base = 'Console'
+	base = 'Console'
 
-executables = [
-    Executable('robinit_console.py', base=base, targetName = 'robinit_console_v1.0.0')
-]
+	executables = [
+	    Executable('robinit_console.py', base=base, targetName = 'robinit_console_v1.0.0')
+	]
 
-setup(name='robinit',
-      version = '1.0.0',
-      description = 'TV Show Tracker and downloader -- console version',
-      options = dict(build_exe = buildOptions),
-      executables = executables)
+	setup(name='robinit',
+	      version = '1.0.0',
+	      description = 'TV Show Tracker and downloader -- console version',
+	      options = dict(build_exe = buildOptions),
+	      executables = executables)
+elif _platform == "win32":
+	from distutils.core import setup
+	import py2exe
+
+	setup(console=['robinit_console.py'])
+
+elif _platformt == "darwin":
+	pass
