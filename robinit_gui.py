df2c5ed1ec71f6ac644562717e458ead584d6520
eximus <fabio_r11@hotmail.com>
2016-09-20 00:07:20 +0100

    changes made to ui to have terminal look

--- robinit_gui.py
+++ robinit_gui.py
@@ -6,10 +6,6 @@ import sys
 
 import gui.mainwindow
 
-#class MainWindow(QDialog, gui.mainwindow.Ui_mainWindow):
-#	def __init__(self, parent = None):
-#		super(gui.mainwindow.Ui_mainWindow, self).__init__(parent)
-
 app = QtGui.QApplication(sys.argv)
 gui_module = gui.mainwindow.QtGui.QMainWindow()
 uiform = gui.mainwindow.Ui_mainWindow()
